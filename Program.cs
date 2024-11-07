20dfbea657bd7b4e325454f463369ea3dcadac13
Janos <kelemen.janos@gmail.com>
2024-11-07 11:54:58 +0000

    health checks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using TplAzureServiceBusWorker;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Prometheus;
 using TplAzureServiceBusWorker.Config;
 using TplAzureServiceBusWorker.Services;
 
@@ -8,8 +9,14 @@ var config = builder.Configuration;
 
 builder.Services.AddOptions();
 builder.Services.Configure<AzureServiceBusConfig>(config.GetSection(nameof(AzureServiceBusConfig)));
+builder.Services.AddHealthChecks()
+    .AddCheck<HealthCheckService>("TPL Bus health check")
+    .ForwardToPrometheus();
 
 builder.Services.AddHostedService<StreamProcessor>();
+
+var metricServer = new KestrelMetricServer(port: 80, url: "/metrics");
+metricServer.Start();
 
 var host = builder.Build();
 host.Run();
---
+++ Services/HealthCheckService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace TplAzureServiceBusWorker.Services
+{
+    public class HealthCheckService : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            //var result = _rnd.Next(5) == 0
+            //? HealthCheckResult.Healthy()
+            //: HealthCheckResult.Unhealthy("Failed random");
+
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
+
